de3d9486d3c4c1e5ee96b11f2df71cecd1217225
Дмитрий Луканин <dylukanin@gmail.com>
2017-01-19 18:04:52 +0300

    fs.ts doc

--- src/fs.ts
+++ src/fs.ts
@@ -1,3 +1,13 @@
+/**
+ * Promise обертка для node fs. Отличается в использовании тем, что нет необходимости
+ * передавать callback и методы возвращают promise.
+ *
+ * fs.readFile('test.txt', 'utf-8').then((text) => {console.log(text)});
+ * или
+ * const text = await fs.readFile('text.txt', 'utf-8');
+ *
+ */
+
 import * as fsNative from 'fs';
 import promisify = require('promisify-node');
 
