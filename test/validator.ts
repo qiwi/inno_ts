de0bb04ebe14a0891aa7c7270ddb24d25da001a2
Lukanin Dmitriy <d.lukanin@qiwi.com>
2017-01-30 11:21:53 +0300

    validator len fixes

--- test/validator.ts
+++ test/validator.ts
@@ -3,15 +3,11 @@ import {Validator, VALIDATION_NO_INT, DEFAULT_CODE} from '../src/validation/vali
 import {ResultError} from "../src/error";
 import {VALIDATION_NO_STRING} from "../src/validation/validator";
 
-describe('validator', function () {
+describe('validator', function() {
     describe('isInt', function() {
         it('numeric string', function() {
             expect(Validator.isInt(" 123123 ")).to.eq(123123);
         });
-
-        // it('number', function() {
-        //     expect(Validator.isInt(100)).to.eq(100);
-        // });
 
         it('boolean', function() {
             expect(function() {Validator.isInt(true)}).to.throw(ResultError);
@@ -26,4 +22,23 @@ describe('validator', function () {
             expect(function() {Validator.isInt(-Infinity)}).to.throw(ResultError);
         });
     });
+
+    describe('isString', function() {
+        it('string', function() {
+            expect(Validator.isString(" 123123 ")).to.eq("123123");
+        });
+
+        it('long string', function() {
+            const arr = new Array(2000);
+            expect(function() {Validator.isString(arr.toString())}).to.throw(ResultError);
+        });
+
+        it('empty string', function() {
+            expect(function() {Validator.isString('')}).to.throw(ResultError);
+        });
+
+        it('spaces', function() {
+            expect(function() {Validator.isString('   ')}).to.throw(ResultError);
+        });
+    })
 });
