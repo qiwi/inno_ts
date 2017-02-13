e77eb632bf2a22268db2b7dd210f83e0580b67a2
Lukanin Dmitriy <d.lukanin@qiwi.com>
2017-02-13 16:40:20 +0300

    removed cache invalidation

--- src/lib/db/pg_pool.ts
+++ src/lib/db/pg_pool.ts
@@ -5,8 +5,6 @@ import * as Pool from 'pg-pool';
 import * as pg from 'pg';
 import * as pgCamelCase from 'pg-camelcase';
 
-delete require.cache[require.resolve('pg')];
-
 pgCamelCase.inject(pg);
 
 export class PgPool extends Pool {
