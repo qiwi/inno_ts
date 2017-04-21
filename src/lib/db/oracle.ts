b8c3cf5181ff96061eaf97eceb690cfe4e052063
Lukanin Dmitriy <d.lukanin@qiwi.com>
2017-04-21 14:16:14 +0300

    oracle connection pool

--- src/lib/db/oracle.ts
+++ src/lib/db/oracle.ts
@@ -5,6 +5,7 @@ import IExecuteOptions = oracledb.IExecuteOptions;
 import IResultSet = oracledb.IResultSet;
 import IConnectionAttributes = oracledb.IConnectionAttributes;
 import {BaseError} from "../error/base";
+import {IConnectionPool} from "oracledb";
 
 export const DB_CONNECT_ERROR: string = 'DB_QUERY';
 export const DB_ORACLE_ERROR: string = 'DB_ORACLE_ERROR';
@@ -15,43 +16,14 @@ export const DB_ORACLE_RELEASE_ERROR: string = 'DB_ORACLE_RELEASE_ERROR';
 // TODO DbError
 
 export class OracleService {
+    public poolMax: number = 50;
+
     protected connectionParams: IConnectionAttributes;
-    protected connection: IConnection;
+    protected pool: IConnectionPool;
 
     constructor(connectionParams: IConnectionAttributes) {
         this.connectionParams = connectionParams;
     }
-
-    /**
-     * Performs connection to database using passed connection params.
-     * @return {Promise<void>}
-     */
-    async connect(): Promise<void> {
-        try {
-            this.connection = await oracledb.getConnection(this.connectionParams);
-            console.debug('Oracle connected');
-        } catch (error) {
-            throw new BaseError({
-                code: DB_CONNECT_ERROR,
-                innerDetails: error.message
-            });
-        }
-    };
-
-    /**
-     * Closes db conn.
-     * @return {Promise<void>}
-     */
-    async disconnect(): Promise<void> {
-        try {
-            await this.connection.release();
-        } catch (error) {
-            throw new BaseError({
-                code: DB_ORACLE_RELEASE_ERROR,
-                innerDetails: error.message
-            });
-        }
-    };
 
     /**
      * Curried {@link getRows} method: getRows with rows prefetch param.
@@ -61,7 +33,7 @@ export class OracleService {
      */
     async getManyRows(query: string, params: Array<any> = []): Promise<IExecuteReturn> {
         try {
-            return await this.connection.execute(query, params, {resultSet: true, prefetchRows: 500});
+            return await this._execute(query, params, {resultSet: true, prefetchRows: 500});
         } catch (error) {
             throw new BaseError({
                 code: DB_ORACLE_ERROR,
@@ -103,10 +75,10 @@ export class OracleService {
     /**
      * Closes result set (optional - closes db connection).
      * @param resultSet
-     * @param closeConnection
+     * @param connection
      * @return {Promise<void>}
      */
-    async closeResultSet(resultSet: IResultSet, closeConnection: boolean = false): Promise<void> {
+    async closeResultSet(resultSet: IResultSet, connection?: IConnection): Promise<void> {
         try {
             await resultSet.close();
         } catch (error) {
@@ -116,8 +88,8 @@ export class OracleService {
             });
         }
 
-        if (closeConnection) {
-            await this.disconnect();
+        if (connection) {
+            await this.disconnect(connection);
         }
     };
 
@@ -130,7 +102,7 @@ export class OracleService {
      */
     async getRows(query: string, params: Array<any> = [], options: IExecuteOptions = {}): Promise<Array<any>> {
         try {
-            const result: IExecuteReturn = await this.connection.execute(query, params, options);
+            const result: IExecuteReturn = await this._execute(query, params, options);
 
             return result.rows;
         } catch (error) {
@@ -144,4 +116,58 @@ export class OracleService {
             });
         }
     };
+
+    /**
+     * Performs connection to database using passed connection params.
+     * @return {Promise<void>}
+     */
+    protected async connect(): Promise<IConnection> {
+        try {
+            if (!this.pool) {
+                this.pool = await oracledb.createPool(
+                    Object.assign({}, this.connectionParams, {poolMax: this.poolMax})
+                );
+            }
+            return await this.pool.getConnection();
+        } catch (error) {
+            throw new BaseError({
+                code: DB_CONNECT_ERROR,
+                innerDetails: error.message
+            });
+        }
+    };
+
+    /**
+     * Closes db conn.
+     * @return {Promise<void>}
+     */
+    protected async disconnect(connection: IConnection): Promise<void> {
+        try {
+            await connection.close();
+        } catch (error) {
+            throw new BaseError({
+                code: DB_ORACLE_RELEASE_ERROR,
+                innerDetails: error.message
+            });
+        }
+    };
+
+    protected async _execute(query: string,
+                             params?: Object | Array<any>,
+                             options?: IExecuteOptions): Promise<IExecuteReturn> {
+        let connection;
+        try {
+            connection = await this.connect();
+            const result = await connection.execute(query, params, options);
+            await this.disconnect(connection);
+
+            return result;
+        } catch (error) {
+            if (connection) {
+                await this.disconnect(connection);
+            }
+
+            throw error;
+        }
+    }
 }
