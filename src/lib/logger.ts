127538e4db004ea3a0462a079ca4c48747516d12
Дмитрий Луканин <dylukanin@gmail.com>
2017-07-08 15:47:27 +0300

    removed any type for logger config

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,12 +1,13 @@
 import * as log4js from 'log4js';
 import Logger = log4js.Logger;
+import {IAppConfig} from './koa/interfaces';
 
 export const DEFAULT_LOG_LEVEL = 'TRACE';
 
 log4js.replaceConsole();
 log4js.setGlobalLogLevel(DEFAULT_LOG_LEVEL);
 
-export function getLogger(config: any): Logger {
+export function getLogger(config: IAppConfig): Logger {
     const logger: Logger = log4js.getLogger();
 
     let logLevel = DEFAULT_LOG_LEVEL;
